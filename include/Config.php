2940d42c1b140c91135b0f83f38a563aefc8866a
Orla Walsh <orlawalsh@outlook.com>
2017-03-20 16:42:09 +0000

    updates

--- include/Config.php
+++ include/Config.php
@@ -2,6 +2,12 @@
 
 /**
  * Database config variables
+ 
+ // mysql -h us-cdbr-iron-east-03.cleardb.net -u b91ef53baf216d -pheroku_4790a1bc4c207ca < android_api.sql 
+
+// mysqldump -h us-cdbr-iron-east-03.cleardb.net -u b91ef53baf216d -p695a46e2 heroku_4790a1bc4c207ca | mysql -h 127.0.0.1 -u owalsh -proot android_api
+
+// mysql --host=us-cdbr-iron-east-03.cleardb.net --user=b91ef53baf216d --password=695a46e2 --reconnect heroku_4790a1bc4c207ca < android_api.sql
  */
 define("DB_HOST", "us-cdbr-iron-east-03.cleardb.net");
 define("DB_USER", "b91ef53baf216d");
@@ -10,8 +16,3 @@ define("DB_DATABASE", "heroku_4790a1bc4c207ca");
 ?>
 
 
-// mysql -h us-cdbr-iron-east-03.cleardb.net -u b91ef53baf216d -pheroku_4790a1bc4c207ca < android_api.sql 
-
-// mysqldump -h us-cdbr-iron-east-03.cleardb.net -u b91ef53baf216d -p695a46e2 heroku_4790a1bc4c207ca | mysql -h 127.0.0.1 -u owalsh -proot android_api
-
-// mysql --host=us-cdbr-iron-east-03.cleardb.net --user=b91ef53baf216d --password=695a46e2 --reconnect heroku_4790a1bc4c207ca < android_api.sql
