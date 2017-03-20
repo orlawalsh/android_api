fc6323656f102d405351f0795de2fb731237c8ab
Orla Walsh <orlawalsh@outlook.com>
2017-03-20 11:23:23 +0000

    Heroku Deployment

---
+++ include/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "us-cdbr-iron-east-03.cleardb.net");
+define("DB_USER", "b91ef53baf216d");
+define("DB_PASSWORD", "695a46e2");
+define("DB_DATABASE", "heroku_4790a1bc4c207ca");
+?>
